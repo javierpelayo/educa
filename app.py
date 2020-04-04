90dbef9d41d1e8c8b7526c91ab91a9e18e53e711
javierpelayo <contact@javierperez.dev>
2020-04-04 03:35:24 -0700

    add flask filter & update UI

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from wtforms import (Form, StringField, TextAreaField,
 from functools import wraps
 from passlib.hash import pbkdf2_sha256
 import os
+
 # create flask app instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['EDUCA_SECRET_KEY']
@@ -23,6 +24,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #DB wraps around our app instance
 db = SQLAlchemy(app)
 
+#################
+# COLOR SCHEMES #
+#################
+#Green: #44AF69
+#Beige: #EFC7C2
+#Red: #F8333C
+#Yellow: #FCAB10
+#Blue: #2B9EB3
 
 ##########
 # ROUTES #
@@ -45,6 +54,18 @@ def packages():
 @app.route('/portal')
 def portal():
     return render_template("portal.html", title="Portal")
+
+###########
+# FILTERS #
+###########
+
+# Queries the browser to force the
+# download of an updated CSS file
+@app.template_filter()
+def autoversion(fn):
+  fp = os.path.join('./', fn[1:])
+  ts = str(os.path.getmtime(fp))
+  return f"{fp}?v={ts}"
 
 if __name__ == "__main__":
     app.run()
