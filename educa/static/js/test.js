01c2f939e1521f7ba217cbfd3f54675bff9c86c9
javierpelayo <contact@javierperez.dev>
2020-05-16 04:16:15 -0700

    add ajax form test functionality with alerts

--- educa/static/js/test.js
+++ educa/static/js/test.js
@@ -2,8 +2,20 @@ let form = document.querySelector('#myForm');
 let errorAlert = document.querySelector('#errorAlert');
 let successAlert = document.querySelector('#successAlert');
 let h1 = document.querySelector('#title');
-let data = "";
-let errors = 0;
+let data;
+let text;
+
+let errorHTML = `<div id="errorAlert" class="col-9 my-1 alert alert-danger alert-dismissible fade show text-center" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>`;
+let successHTML = `<div id="successAlert" class="col-9 my-1 alert alert-success alert-dismissible fade show text-center" role="alert">
+		              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>`;
+
 // Create an Ajax request
 let request = new XMLHttpRequest();
 
@@ -21,34 +33,35 @@ function fn(){
     request.onreadystatechange = function() {
       if (request.readyState == XMLHttpRequest.DONE) {
         data = JSON.parse(request.responseText);
+
+				// checks if the alerts exist(in-case user has dismissed them)
+				errorAlert = document.querySelector('#errorAlert');
+				successAlert = document.querySelector('#successAlert');
+
+				// if the request has an error
         if(data.error){
-          error = `<div id="errorAlert" class="col-9 my-1 alert alert-danger alert-dismissible fade show text-center" role="alert" style="display: none;">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>`;
-          
-          h1.insertAdjacentHTML('afterend', error);
+					// if the alert already exists
+					if(errorAlert) {
+						errorAlert.parentNode.removeChild(errorAlert);
+					} else if(successAlert) {
+						successAlert.parentNode.removeChild(successAlert);
+					}
+
+          h1.insertAdjacentHTML('afterend', errorHTML);
           errorAlert = document.querySelector('#errorAlert');
-          let text = document.createTextNode(data.error);
-          if (errors < 1) {
-            errorAlert.appendChild(text);
-          }
-          errorAlert.style.display = "block";
-          successAlert.style.display = "none";
-          errors++;
+          text = document.createTextNode(data.error);
+          errorAlert.appendChild(text);
         } else {
-          success = `<div id="successAlert" class="col-9 my-1 alert alert-success alert-dismissible fade show text-center" role="alert" style="display: none;">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>`;
-          h1.insertAdjacentHTML('afterend', success);
+					if(successAlert) {
+						successAlert.parentNode.removeChild(successAlert);
+					} else if (errorAlert) {
+						errorAlert.parentNode.removeChild(errorAlert);
+					}
+
+          h1.insertAdjacentHTML('afterend', successHTML);
           successAlert = document.querySelector('#successAlert');
-          let text = document.createTextNode(data.name);
+          text = document.createTextNode(data.name);
           successAlert.appendChild(text);
-          successAlert.style.display = "block";
-          errorAlert.style.display = "none";
         }
       }
     }
