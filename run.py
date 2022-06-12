692cc06a3ee8fb4fbbccb6da95474c96be81a559
javierpelayo <contact@javierperez.dev>
2022-06-12 15:57:36 -0700

    Update var port

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from app import create_app, db
 from flask_migrate import Migrate
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 app = create_app()
 with app.app_context():
@@ -8,4 +11,4 @@ with app.app_context():
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
