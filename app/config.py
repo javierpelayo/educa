7445d2f4b0e8ea3ce5b82c65d09d69e2261124cc
javierpelayo <contact@javierperez.dev>
2022-06-12 15:28:46 -0700

    edit retrieval of config vars

--- app/config.py
+++ app/config.py
@@ -1,15 +1,20 @@
-import configparser
+# import configparser
+import os
 
-config = configparser.ConfigParser()
-config.read('app/config.ini')
+# config = configparser.ConfigParser()
+# config.read('app/config.ini')
 
 class Config:
-    SECRET_KEY = config['DEFAULT']['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = config['DEFAULT']['DATABASE_URI']
+    # SECRET_KEY = config['DEFAULT']['SECRET_KEY']
+    # SQLALCHEMY_DATABASE_URI = config['DEFAULT']['DATABASE_URI']
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 500 * 1024
     MAIL_SERVER = 'mail.privateemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config['DEFAULT']['EMAIL_USER']
-    MAIL_PASSWORD = config['DEFAULT']['EMAIL_PASSWORD']
+    # MAIL_USERNAME = config['DEFAULT']['EMAIL_USER']
+    # MAIL_PASSWORD = config['DEFAULT']['EMAIL_PASSWORD']
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
